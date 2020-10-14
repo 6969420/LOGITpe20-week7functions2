42532b4b9954b2507ad20cd9d855c2175d405bcd
Opilane TTHK <opilane@tthk.ee>
2020-10-14 16:24:47 +0300

    week7

---
+++ Week7Converters/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Celsius_to_Fahrenheit_consversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double f, c;
+            Console.WriteLine("Enter the value of Celsius:");
+            c = Convert.ToDouble(Console.ReadLine());
+            f = c * 9 / 5 + 32;
+            Console.WriteLine(c + "°C in Fahrenheit is: " + f + "°F");
+            Console.WriteLine("Enter the value of Fahrenheit:");
+            f = Convert.ToDouble(Console.ReadLine());
+            c = (f - 32) * 5 / 9;
+            Console.WriteLine(f + "°F in Celsius is: " + c + "°C");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ šomething/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace šomething
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter C for Fahrenheit to Celsius or f for Celsius to Fahrenheit");
+            string userInput = Console.ReadLine();
+
+            if(userInput == "C")
+            {
+                ToCelsius();
+            }else
+            {
+                ToFahrenheit();
+            }
+        }
+        public static void ToCelsius()
+        {
+            Console.WriteLine("enter the temperature in F: ");
+            int tempF = Int32.Parse(Console.ReadLine());
+            double result = (tempF - 32) * 5 / 9;
+            Console.WriteLine($"Result in Celcius: {result}");
+        }
+
+        public static void ToFahrenheit()
+        {
+            Console.WriteLine("enter the temperature in C: !");
+            int tempC = Int32.Parse(Console.ReadLine());
+            double result = (tempC * 9) / 5 + 32;
+            Console.WriteLine($"Result in Fahrenheit: {result}");
+        }
+    }
+}
